71390ae835f13a312042556a80f77334652d6513
paixtechdom <paixtechdom@gmail.com>
2024-06-13 23:29:55 +0100

    continued home page

--- src/pages/home/sections/Hero.tsx
+++ src/pages/home/sections/Hero.tsx
@@ -2,7 +2,7 @@ import { Button } from "../../../assets/components/Button"
 import hero from "../../../assets/images/hero.png"
 export const Hero: React.FC = () => {
     return(
-        <div className="min-h-screen bg-white w-full center relative mt-[15vh] lg:mt-0"> 
+        <div className="min-h-screen bg-white w-full center relative mt-[12vh] md:mt-[15vh] lg:mt-0"> 
             <div className="w-11/12 lg:w-10/12 flex flex-col lg:flex-row items-center justify-between z-10 gap-[50px]">
                 <div className="flex flex-col gap-5 w-full lg:w-5/12">
                     <h1 className="font-bold text-5xl text-primary">This and that</h1>
