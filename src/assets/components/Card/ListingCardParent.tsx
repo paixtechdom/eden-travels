00c86f6a065f17b99812e4041b7a1dbbc47a7d7e
paixtechdom <paixtechdom@gmail.com>
2024-06-18 03:26:19 +0100

    fixed miscellaneous

--- src/assets/components/Card/ListingCardParent.tsx
+++ src/assets/components/Card/ListingCardParent.tsx
@@ -8,7 +8,7 @@ export const ListingCardParent:FC<ListingCardParentInterface> = ({data, heading,
         <div id={heading?.replace(" ", "")?.replace(" ", "")?.replace(" ", "")?.replace(" ", "")} className="w-full center flex-col gap-9 pt-[20vh]">
             <div className="flex flex-col items-center gap-2 w-11/12 lg:w-10/12 text-secondary">   
                 <h2 className="font-semibold text-left text-4xl w-full">{heading}</h2>  
-                <div className="flex flex-col gap-3 w-full">
+                <div className="flex flex-col gap-3 w-full tracking-wide leading-relaxed">
                     {
                         typeof(desc) == "string" ? 
                         <p className="w-full">{desc}</p>
